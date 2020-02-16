053f1f74891fd4039c6d1f3d3daa69e3ddb23e95
Noe Rivals <noe.rivals@epitech.eu>
2020-02-16 19:16:31 +0100

    [+](main.go): improve performance debug

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"runtime/pprof"
@@ -57,9 +58,6 @@ func getEthKeys(maxLoad uint) (map[string]Key, error) {
 func try(keys map[string]Key) (bool, *Key) {
 	key := GenerateKey()
 	keyFound, exist := keys[key.address]
-	if key.private[0] == 3 && key.private[1] == 2 {
-		exist = true
-	}
 	if exist {
 		key.value = keyFound.value
 		println("Found one key!")
@@ -79,30 +77,43 @@ func infiniteWorker(start chan bool, done chan *Key, keys map[string]Key) {
 	}
 }
 
-func compute(keys map[string]Key, nbThread uint, savepath string) {
+func getTimeEstimationForOneKey(keyLoaded uint, nbGeneratedPerMinute uint) string {
+	timeToBreakOneKeyMin := 1.5e+48 / float64(keyLoaded) / float64(nbGeneratedPerMinute)
+	if timeToBreakOneKeyMin > 290*350*24*60 { // check if the duration isn't going to overflow
+		fstr := fmt.Sprintf("%e", timeToBreakOneKeyMin)
+		return ">290y (" + fstr + "m)"
+	}
+	t := time.Duration(time.Minute * time.Duration(timeToBreakOneKeyMin))
+	return t.String()
+}
+
+func compute(keys map[string]Key, nbThread uint, savepath string, debugtime uint) {
 	nbTried := 0
-	startTime := time.Now()
 	start := make(chan bool)
 	done := make(chan *Key)
 	for i := 0; i < int(nbThread); i++ {
 		go infiniteWorker(start, done, keys)
 		start <- true
 	}
+	timeout := time.After(time.Second * time.Duration(debugtime))
 	for {
-		key := <-done
-		start <- true
-		nbTried++
-		if key != nil {
-			key.Debug()
-			if err := key.Save(savepath); err != nil {
-				println(err)
-				panic(err)
+		select {
+		case key := <-done:
+			start <- true
+			nbTried++
+			if key != nil {
+				key.Debug()
+				if err := key.Save(savepath); err != nil {
+					println(err)
+					panic(err)
+				}
 			}
-		}
-		if nbTried%10000 == 0 {
-			println(time.Since(startTime).String())
-			startTime = time.Now()
-			println("Tried: ", nbTried)
+		case <-timeout:
+			fmt.Printf("Tested %d keys in %ds\n", nbTried, debugtime)
+			estimation := getTimeEstimationForOneKey(uint(len(keys)), uint(nbTried))
+			fmt.Printf("Estimated time to crack one key %s\n", estimation)
+			nbTried = 0
+			timeout = time.After(time.Second * time.Duration(debugtime))
 		}
 	}
 }
@@ -123,9 +134,10 @@ func haveWriteAccess(path string) error {
 
 func main() {
 	nbthread := flag.Uint("thread", 2, "Number of threads to use")
-	maxkeyloaded := flag.Uint("maxKeys", 10, "Max keys to load in memory")
+	maxkeyloaded := flag.Uint("maxKeys", 0, "Max keys to load in memory")
 	cpuprofile := flag.String("cpuprofile", "", "write cpu profile to file")
 	savepath := flag.String("savepath", "./keys_found", "Path to save keys")
+	debugevery := flag.Uint("debugtime", 10, "time in seconds between two performance debug")
 	flag.Parse()
 	if *cpuprofile != "" {
 		f, err := os.Create(*cpuprofile)
@@ -145,5 +157,5 @@ func main() {
 		println(err.Error())
 		os.Exit(1)
 	}
-	compute(keys, *nbthread, *savepath)
+	compute(keys, *nbthread, *savepath, *debugevery)
 }
