06d41b6214d0f2a833aaf8fb3f28bec24f85a6a9
Noe Rivals <noe.rivals@epitech.eu>
2020-02-16 04:06:24 +0100

    [-]: fixed the way address was managed in memory and add infinite worker

--- key.go
+++ key.go
@@ -2,46 +2,44 @@ package main
 
 import (
 	"crypto/ecdsa"
-	"fmt"
+	"encoding/hex"
 	"log"
 
-	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
 // Key is a struct that hold ethereum keys address
 type Key struct {
-	private uint32
-	public  uint64
-	address uint32 // 20 bits
-	value   uint32
+	private []byte
+	public  []byte
+	address string
+	value   uint64
 }
 
-// Generate a random ethereum key
-func Generate() *Key {
+// Debug a key
+func (key *Key) Debug() {
+	println("Key:")
+	println("  private: ", hex.EncodeToString(key.private))
+	println("  public: ", hex.EncodeToString(key.public))
+	println("  address: ", key.address)
+	println("  amount: ", key.value)
+}
+
+// GenerateKey generate a random ethereum key
+func GenerateKey() *Key {
+	key := Key{}
 	privateKey, err := crypto.GenerateKey()
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	privateKeyBytes := crypto.FromECDSA(privateKey)
-	fmt.Println(hexutil.Encode(privateKeyBytes)[2:]) // 0xfad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19
-
+	key.private = crypto.FromECDSA(privateKey)
 	publicKey := privateKey.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
 	if !ok {
 		log.Fatal("error casting public key to ECDSA")
 	}
-
-	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
-	fmt.Println(hexutil.Encode(publicKeyBytes)[4:]) // 0x049a7df67f79246283fdc93af76d4f8cdd62c4886e8cd870944e817dd0b97934fdd7719d0810951e03418205868a5c1b40b192451367f28e0088dd75e15de40c05
-
-	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
-	fmt.Println(address) // 0x96216849c49358B10257cb55b28eA603c874b05E
-
-	hash := sha3.NewKeccak256()
-	hash.Write(publicKeyBytes[1:])
-	fmt.Println(hexutil.Encode(hash.Sum(nil)[12:])) // 0x96216849c49358b10257cb55b28ea603c874b05e
-	key := Key{}
+	key.public = crypto.FromECDSAPub(publicKeyECDSA)
+	address := crypto.PubkeyToAddress(*publicKeyECDSA)
+	key.address = address.Hex()[2:]
 	return &key
 }
--- main.go
+++ main.go
@@ -2,29 +2,27 @@ package main
 
 import (
 	"bufio"
-	"encoding/binary"
-	"encoding/hex"
+	"flag"
+	"log"
 	"os"
+	"runtime/pprof"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func loadKeyFromLine(line string) (*Key, error) {
 	infos := strings.Split(line, ",")
-	addressString := strings.ReplaceAll(infos[0], " ", "")
-	address, err := hex.DecodeString(addressString[2:])
+	address := infos[0][2:]
+	value, err := strconv.ParseUint(infos[1], 10, 64)
 	if err != nil {
 		return nil, err
 	}
-	value, err := strconv.Atoi(infos[1])
-	if err != nil {
-		return nil, err
-	}
-	key := Key{address: binary.BigEndian.Uint32(address), value: uint32(value)}
+	key := Key{address: address, value: value}
 	return &key, nil
 }
 
-func getEthKeys() (map[uint32]Key, error) {
+func getEthKeys(maxLoad uint) (map[string]Key, error) {
 	fileName := "./keys.csv"
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -33,7 +31,7 @@ func getEthKeys() (map[uint32]Key, error) {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
-	keys := make(map[uint32]Key)
+	keys := make(map[string]Key)
 
 	scanner.Scan()
 	for lineIdx := 0; scanner.Scan(); lineIdx++ {
@@ -41,7 +39,7 @@ func getEthKeys() (map[uint32]Key, error) {
 		if lineIdx == 0 {
 			continue
 		}
-		if lineIdx == 10 {
+		if maxLoad != 0 && uint(lineIdx) > maxLoad {
 			break
 		}
 		key, err := loadKeyFromLine(line)
@@ -53,15 +51,63 @@ func getEthKeys() (map[uint32]Key, error) {
 	return keys, nil
 }
 
-func compute(map[uint32]Key) {
+func try(keys map[string]Key) (bool, *Key) {
+	key := GenerateKey()
+	keyFound, exist := keys[key.address]
+	if exist {
+		key.value = keyFound.value
+		println("Found one key!")
+		key.Debug()
+	}
+	return exist, key
+}
 
+func infiniteWorker(start chan bool, done chan bool, keys map[string]Key) {
+	for {
+		<-start
+		found, _ := try(keys)
+		done <- found
+	}
+}
+
+func compute(keys map[string]Key, nbThread uint) {
+	nbTried := 0
+	startTime := time.Now()
+	start := make(chan bool)
+	done := make(chan bool)
+	for i := 0; i < int(nbThread); i++ {
+		go infiniteWorker(start, done, keys)
+		start <- true
+	}
+	for {
+		<-done
+		start <- true
+		nbTried++
+		if nbTried%10000 == 0 {
+			println(time.Since(startTime).String())
+			startTime = time.Now()
+			println("Tried: ", nbTried)
+		}
+	}
 }
 
 func main() {
-	keys, err := getEthKeys()
+	nbthread := flag.Uint("thread", 2, "Number of threads to use")
+	maxkeyloaded := flag.Uint("maxKeys", 10, "Max keys to load in memory")
+	cpuprofile := flag.String("cpuprofile", "", "write cpu profile to file")
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+	keys, err := getEthKeys(*maxkeyloaded)
 	if err != nil {
 		println(err.Error())
 		os.Exit(1)
 	}
-	compute(keys)
+	compute(keys, *nbthread)
 }
